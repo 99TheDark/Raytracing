37112c6da0b72f1750ccd214bc6be00c863c974b
TheDark <104472362+99TheDark@users.noreply.github.com>
2023-04-10 00:43:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ Promise.all([
     .then(files => Promise.all(
         files.map(file => file.text())
     ))
-    .then(texts => main(texts[0], texts[1]))
+    .then(texts => main.apply(null, texts))
 
 const scene = [
-    0, 0, 0, 0, 0
+    0, 0, 0, 0, 0, 0, 0
 ];
+
+const constants = {
+    NUM_SPHERES: scene.length,
+    NUM_LIGHTS: 1,
+    MAX_BOUNCE: MAX_LIGHT_BOUNCES,
+    EPSILON: EPSILON,
+    PI: PI,
+    HALF: HALF,
+    TAU: TAU
+};
 
 var main = (vertexSource, fragmentSource) => {
     const canvas = document.getElementById("canvas");
@@ -17,12 +27,7 @@ var main = (vertexSource, fragmentSource) => {
     const gl = canvas.getContext("webgl2", {antialias: true});
     const fps = document.getElementById("fps");
 
-    fragmentSource = fragmentSource
-        .replaceAll("NUM_SPHERES", scene.length)
-        .replaceAll("MAX_BOUNCE", MAX_LIGHT_BOUNCES)
-        .replaceAll("INFINITY", INFINITY)
-        .replaceAll("EPSILON", EPSILON)
-        .replaceAll("INFINITY", INFINITY)
+    for(let name in constants) fragmentSource = fragmentSource.replaceAll(name, constants[name]);
 
     const [width, height] = [parseInt(canvasStyle.width) / RESOLUTION, parseInt(canvasStyle.height) / RESOLUTION];
     const large = Math.max(width, height);
@@ -90,6 +95,8 @@ var main = (vertexSource, fragmentSource) => {
 
     fps.textContent = "60fps";
 
+    let start = false;
+
     let keys = {};
 
     let cam = {
@@ -98,34 +105,51 @@ var main = (vertexSource, fragmentSource) => {
         speed: 0.3
     };
 
+    let img = new ImageData(width, height);
+
+    let buffer = new Uint8ClampedArray(width * height * 4);
+
+    let frame = 0;
+
     let last = performance.now();
-    const draw = () => {
+    const draw = begin => {
         let time = performance.now() / 1000;
 
-        cam.dir.x %= TAU;
-        cam.dir.y %= TAU;
-
-        cam.dir.y = Math.max(Math.min(cam.dir.y, HALF), - HALF);
-
-        if(keys.w) {
-            cam.pos.x += Math.cos(cam.dir.x) * cam.speed;
-            cam.pos.z += Math.sin(cam.dir.x) * cam.speed;
+        if(start) {
+            if(keys.w) {
+                cam.pos.x += Math.cos(cam.dir.x) * cam.speed;
+                cam.pos.z += Math.sin(cam.dir.x) * cam.speed;
+            }
+            if(keys.s) {
+                cam.pos.x -= Math.cos(cam.dir.x) * cam.speed;
+                cam.pos.z -= Math.sin(cam.dir.x) * cam.speed;
+            }
+            if(keys.d) {
+                cam.pos.x += Math.cos(cam.dir.x + HALF) * cam.speed;
+                cam.pos.z += Math.sin(cam.dir.x + HALF) * cam.speed;
+            }
+            if(keys.a) {
+                cam.pos.x -= Math.cos(cam.dir.x + HALF) * cam.speed;
+                cam.pos.z -= Math.sin(cam.dir.x + HALF) * cam.speed;
+            }
         }
-        if(keys.s) {
-            cam.pos.x -= Math.cos(cam.dir.x) * cam.speed;
-            cam.pos.z -= Math.sin(cam.dir.x) * cam.speed;
-        }
-        if(keys.d) {
-            cam.pos.x += Math.cos(cam.dir.x + HALF) * cam.speed;
-            cam.pos.z += Math.sin(cam.dir.x + HALF) * cam.speed;
-        }
-        if(keys.a) {
-            cam.pos.x -= Math.cos(cam.dir.x + HALF) * cam.speed;
-            cam.pos.z -= Math.sin(cam.dir.x + HALF) * cam.speed;
-        }
+        
+        let texture = gl.createTexture();
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
 
         const timeUniformLocation = gl.getUniformLocation(program, "time");
         gl.uniform1f(timeUniformLocation, time);
+
+        const frameUniformLocation = gl.getUniformLocation(program, "frame");
+        gl.uniform1i(frameUniformLocation, frame);
+
+        const movingUniformLocation = gl.getUniformLocation(program, "moving");
+        gl.uniform1i(movingUniformLocation, start ? 1 : 0);
 
         const camPosUniformLocation = gl.getUniformLocation(program, "cam.pos");
         const camDirUniformLocation = gl.getUniformLocation(program, "cam.dir");
@@ -137,8 +161,15 @@ var main = (vertexSource, fragmentSource) => {
 
         gl.drawArrays(gl.TRIANGLES, 0, 6);
 
-        fps.textContent = `${Math.round(1000 / (performance.now() - last))}fps`;
+        if(!start) {
+            gl.readPixels(0, 0, width, height, gl.RGBA, gl.UNSIGNED_BYTE, buffer);
+            img.data.set(buffer);
+        }
+        
+        fps.textContent = `${Math.round(1000 / (performance.now() - begin))}fps`;
         last = performance.now();
+
+        frame++;
 
         requestAnimationFrame(draw);
     };
@@ -154,11 +185,22 @@ var main = (vertexSource, fragmentSource) => {
     });
 
     document.addEventListener("mousemove", e => {
-        cam.dir.x += e.movementX / large;
-        cam.dir.y -= e.movementY / large;
+        if(start) {
+            cam.dir.x += e.movementX / large;
+            cam.dir.y -= e.movementY / large;
+
+            cam.dir.x %= TAU;
+            cam.dir.y %= TAU;
+
+            cam.dir.y = Math.max(Math.min(cam.dir.y, HALF), - HALF);
+        }
     });
 
     canvas.addEventListener("click", () => {
+        start = true;
+        img = new ImageData(width, height);
+        frame = 0;
+
         canvas.requestPointerLock();
     });
 
